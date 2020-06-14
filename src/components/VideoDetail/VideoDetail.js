ecfd22d665d8b57165e9745d4111f93364cc02d2
Takayuki Tooyama <t.takayuki.0107@gmail.com>
2020-06-14 11:54:29 +0900

    URLの変更時にも実行できるよに変更

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -31,7 +31,7 @@ const VideoDetail = () => {
   useEffect(() => {
     setSelectedVideo();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [location.search]);
   return globalState.selected && globalState.selected.id ? (
     <div className={Style.wrap}>
       <VideoPlay id={globalState.selected.id} />
